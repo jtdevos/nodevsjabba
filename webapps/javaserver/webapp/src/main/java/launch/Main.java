50dab9b6b2ec53d43150ffff47310fce09fbfa9d
Jim deVos <jim.devos@gmail.com>
2016-09-26 17:41:48 -0700

    fixing file paths, putting tomcat basedir in targetdir

--- webapps/javaserver/webapp/src/main/java/launch/Main.java
+++ webapps/javaserver/webapp/src/main/java/launch/Main.java
@@ -7,17 +7,20 @@ import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 /**
  * Created by JAMES on 9/24/2016.
  */
 public class Main {
-
     public static void main(String[] args) throws Exception {
         File webappDir = Paths.get("src/main/webapp/").toFile();
         Tomcat tomcat = new Tomcat();
         String port = System.getenv("PORT");
+
+        Path basedir = Paths.get("target/tomcatbase");
+        tomcat.setBaseDir(basedir.toFile().getAbsolutePath());
 
         if(port == null) {
             port = "8080";
--- webapps/javaserver/webapp/src/main/java/us/youfailed/webapp/ListngServlet.java
+++ webapps/javaserver/webapp/src/main/java/us/youfailed/webapp/ListngServlet.java
@@ -3,16 +3,10 @@ package us.youfailed.webapp;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.nio.file.*;
 
 /**
  * Created by JAMES on 9/24/2016.
@@ -23,14 +17,20 @@ public class ListngServlet extends HttpServlet {
     ServletOutputStream out;
     HttpServletRequest request;
     HttpServletResponse response;
+    Path filesPath;
+    File filesDir;
 
 
-    String strFilesDir = System.getenv("OUTFILESDIR");
-    Path filesPath = Paths.get(strFilesDir);
-    File filesDir = Paths.get(strFilesDir).toFile();
 
     public ListngServlet() {
-        if(strFilesDir == null) strFilesDir = "c:\\temp";
+        String strFilesDir = System.getenv("OUTFILESDIR");
+
+        if(strFilesDir == null) {
+            strFilesDir = System.getProperty("java.io.tmpdir");
+            System.err.println(String.format("OUTFILESDIR not defined. Using this instead: %s", strFilesDir));
+        }
+        filesPath = Paths.get(strFilesDir);
+        filesDir = Paths.get(strFilesDir).toFile();
     }
 
     @Override
